98c6747e04a1ef1840193051e0d2be20411f8c47
Denis Keller <denis.keller@xtp-group.com>
2025-01-22 13:53:12 +0100

    changes

--- send_email.php
+++ send_email.php
@@ -1,42 +1,29 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $company = htmlspecialchars($_POST['company']);
+    $investorType = htmlspecialchars($_POST['investor-type']);
+    $aum = htmlspecialchars($_POST['aum']);
 
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
+    $to = "contact@sintro.eu" ;
+    $subject = "New Investor Inquiry from $name";
+    $message = "
+    Name: $name\n
+    Email: $email\n
+    Phone: $phone\n
+    Company Name: $company\n
+    Investor Type: $investorType\n
+    Company AUM: $aum\n
+    ";
+    $headers = "From: no-reply@sintro.eu\r\n";
+    $headers .= "Reply-To: $email\r\n";
 
-$mail = new PHPMailer(true); // Passing `true` enables exceptions
-
-try {
-    // Server settings
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.goneo.de';                        // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'no-replyl@sintro.eu';              // SMTP username
-    $mail->Password = 'Rwentz37-';                     // SMTP password
-    $mail->SMTPSecure = 'ssl';                            // Enable SSL encryption, `tls` also accepted with port 587
-    $mail->Port = 465;                                    // TCP port to connect to
-
-    // Recipients
-    $mail->setFrom('no-reply@sintro.eu', 'Sintro Contact Form');
-    $mail->addAddress('contact@sintro.eu', 'Sintro Contact');     // Add a recipient
-
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = "New Inquiry from Website";
-    $mail->Body    = "Name: " . htmlspecialchars($_POST['name']) . "<br>" .
-                     "Email: " . htmlspecialchars($_POST['email']) . "<br>" .
-                     "Phone: " . htmlspecialchars($_POST['phone']) . "<br>" .
-                     "Company Name: " . htmlspecialchars($_POST['company']) . "<br>" .
-                     "Investor Type: " . htmlspecialchars($_POST['investor-type']) . "<br>" .
-                     
-                     "Company AUM: " . htmlspecialchars($_POST['aum']) . "<br>";
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Your message has been sent successfully.";
+    } else {
+        echo "There was an error sending your message. Please try again later or send a mail to contact@sintro.eu.";
+    }
 }
 ?>

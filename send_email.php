d0622ed1aa52d117ecfc6a0700cdebdb005c9f46
Denis Keller <denis.keller@xtp-group.com>
2025-03-04 16:09:50 +0100

    Java Script

--- send_email.php
+++ send_email.php
@@ -1,6 +1,4 @@
 <?php
-session_start(); // Start the session at the very beginning
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -15,41 +13,37 @@ require 'PHPMailer/src/SMTP.php';
 $mail = new PHPMailer(true);
 
 try {
-    // Server settings
+    // Servereinstellungen
     $mail->isSMTP();
     $mail->Host = 'smtp.goneo.de';
     $mail->SMTPAuth = true;
     $mail->Username = 'no-reply@sintro.eu';
-    $mail->Password = 'Dk366049551'; // Replace with your real password
+    $mail->Password = 'Dk366049551';
     $mail->SMTPSecure = 'ssl';
     $mail->Port = 465;
 
-    // Recipients
+    // Empfänger
     $mail->setFrom('no-reply@sintro.eu', 'SINTRO Website Contact Form');
     $mail->addAddress('contact@sintro.eu');
 
-    // Email content
+    // E-Mail Inhalt
     $mail->isHTML(true);
-    $mail->Subject = 'New contact request from the website';
-    $mail->Body    = 'Here are the details:<br>' .
+    $mail->Subject = 'Neue Kontaktanfrage von der Website';
+    $mail->Body    = 'Hier sind die Details:<br>' . 
                      'Name: ' . htmlspecialchars($_POST['name']) . '<br>' .
                      'Email: ' . htmlspecialchars($_POST['email']) . '<br>' .
-                     'Phone: ' . htmlspecialchars($_POST['phone']) . '<br>' .
-                     'Company: ' . htmlspecialchars($_POST['company']) . '<br>' .
-                     'Investor Type: ' . htmlspecialchars($_POST['investor-type']);
+                     'Telefon: ' . htmlspecialchars($_POST['phone']) . '<br>' .
+                     'Firma: ' . htmlspecialchars($_POST['company']) . '<br>' .
+                     'Investortyp: ' . htmlspecialchars($_POST['investor-type']);
 
-    if ($mail->send()) {
-        $_SESSION['message'] = "Your message has been sent successfully. Thank you for your inquiry and interest in our organization. Your request is being handled with top priority and we will process it promptly. Our dedicated team will get back to you shortly.";
-    } else {
-        $_SESSION['message'] = "There was an error sending your message. Please try again later or send a mail to contact@sintro.eu.";
-    }
-    // Redirect back to the form page (or to any specific page you want)
-    header("Location: index.php"); // Make sure to specify the correct path to your form page
-    exit;
+    $mail->send();
+    echo 'Your message has been sent successfully. Thank you for your inquiry and interest in our organization. Your request is be handled with top priority and we will process it promptly. Our dedicated team will get back to you shortly.
+
+With kind regards,
+
+Your SINTRO Service Team
+';
 } catch (Exception $e) {
-    $_SESSION['message'] = "There was an error sending your message. Please try again later or send a mail to contact@sintro.eu. " . $mail->ErrorInfo;
-    // Redirect even if an exception occurs
-    header("Location: index.php"); // Make sure to specify the correct path to your form page
-    exit;
+    echo 'There was an error sending your message. Please try again later or send a mail to contact@sintro.eu. ' . $mail->ErrorInfo;
 }
 ?>
